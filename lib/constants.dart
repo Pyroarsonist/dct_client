5cfe82b12664d74760bea672b074282aa6925a6d
Pyroarsonist <velikiydan@gmail.com>
2021-04-13 18:51:46 +0300

    added heatmaps

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 const SEND_GEODATA_JOB = 'send-geodata';
 const SEND_GEODATA_TAG = 'send-geodata-tag';
+//todo: uncomment
 // const SEND_GEODATA_DURATION_INTERVAL = const Duration(seconds: 15);
 const SEND_GEODATA_DURATION_INTERVAL = const Duration(seconds: 4);
 const APP_TITLE = "Dangerous crowds tracker";
+const DEFAULT_MAPS_ZOOM = 17.5;
--- lib/geolocation/map_widget.dart
+++ lib/geolocation/map_widget.dart
@@ -4,7 +4,9 @@ import 'package:dct_client/jobs/get_status_and_locations.job.dart';
 import 'package:dct_client/jobs/send-geodata.job.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 
 import '../constants.dart';
 import 'dtos/get_locations.dto.dart';
@@ -20,6 +22,9 @@ class _MapWidgetState extends State<MapWidget> {
   StreamSubscription<Position> _positionStreamSubscription;
   Position _position;
   CrowdStatus _status;
+
+  //todo: refactor
+  Set<Heatmap> _heatmaps = {};
 
   //todo: refactor
   Set<Circle> _circles = {};
@@ -72,36 +77,73 @@ class _MapWidgetState extends State<MapWidget> {
         _position = position;
         _status = dto.status;
 
-        _circles = dto.locations.asMap().entries.map((entry) {
+        // _circles = dto.locations.asMap().entries.map((entry) {
+        //   var l = entry.value;
+        //   var i = entry.key;
+        //
+        //   var circle = Circle(
+        //     circleId: CircleId("circle_id:$i"),
+        //     strokeColor: Colors.red,
+        //     fillColor: Colors.orange,
+        //     strokeWidth: 2,
+        //     center: LatLng(l.latitude, l.longitude),
+        //     radius: 15,
+        //   );
+        //
+        //   return circle;
+        // }).toSet();
+
+        _heatmaps = dto.locations.asMap().entries.map((entry) {
           var l = entry.value;
           var i = entry.key;
 
-          var circle = Circle(
-            circleId: CircleId("circle_id:$i"),
-            strokeColor: Colors.red,
-            fillColor: Colors.orange,
-            strokeWidth: 2,
-            center: LatLng(l.latitude, l.longitude),
-            radius: 15,
-          );
+          // var circle = Heatmap(
+          //     heatmapId: HeatmapId("heatmap_id:$i"),
+          //     radius: 20,
+          //     points: _createPoints(LatLng(l.latitude, l.longitude)),
+          //     gradient: HeatmapGradient(
+          //         colors: <Color>[Colors.green, Colors.red],
+          //         startPoints: <double>[0.2, 0.8]));
+
+          var circle = Heatmap(
+              heatmapId: HeatmapId("heatmap_id:$i"),
+              radius: 30,
+              points: _createPoints(LatLng(l.latitude, l.longitude)),
+              gradient: HeatmapGradient(
+                  colors: <Color>[Colors.green, Colors.red],
+                  startPoints: <double>[0.2, 0.8]));
 
           return circle;
         }).toSet();
       });
 
       // //todo: refactor
-      // final GoogleMapController controller = await _controller.future;
+      //   final GoogleMapController controller = await _controller.future;
       //
-      // var cameraPosition = CameraPosition(
-      //   target: LatLng(
-      //     position.latitude,
-      //     position.longitude,
-      //   ),
-      //   zoom: 15,
-      // );
+      //   var cameraPosition = CameraPosition(
+      //     target: LatLng(
+      //       position.latitude,
+      //       position.longitude,
+      //     ),
+      //     zoom: DEFAULT_MAPS_ZOOM,
+      //   );
       //
-      // controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      //   controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
     });
+  }
+
+  List<WeightedLatLng> _createPoints(LatLng location) {
+    final List<WeightedLatLng> points = <WeightedLatLng>[];
+    for (var i = -1; i <= 100; i++)
+      for (var j = -1; j <= 1; j++)
+        points.add(_createWeightedLatLng(
+            location.latitude + i, location.longitude + j, 1));
+
+    return points;
+  }
+
+  WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
+    return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
   }
 
   @override
@@ -110,7 +152,7 @@ class _MapWidgetState extends State<MapWidget> {
       target:
           //todo: fix null
           LatLng(_position?.latitude ?? 0, _position?.longitude ?? 0),
-      zoom: 15,
+      zoom: DEFAULT_MAPS_ZOOM,
     );
 
     if (_position == null) {
@@ -135,6 +177,7 @@ class _MapWidgetState extends State<MapWidget> {
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
           circles: _circles,
+          heatmaps: _heatmaps,
         ),
       ),
     ));
