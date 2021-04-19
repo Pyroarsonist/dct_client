72e0307df7dd9c06c73440f73c8a215b6a64901d
Pyroarsonist <velikiydan@gmail.com>
2021-04-19 22:36:24 +0300

    refactored auth forms

--- lib/auth/login_widget.dart
+++ lib/auth/login_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dct_client/services/auth_service.dart';
 import 'package:dct_client/services/token_service.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 import '../logger.dart';
 import '../routes.dart';
@@ -13,16 +14,28 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginWidget> {
-  String email;
-  String password;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  static const sizedBoxHeight = 30.0;
+  static const _sizedBoxHeight = 30.0;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   Future<void> _login(BuildContext context) async {
     try {
+      if (!_formKey.currentState.validate()) {
+        return;
+      }
+
       final dto = LoginDto(
-        email,
-        password,
+        _emailController.text,
+        _passwordController.text,
       );
 
       final response = await AuthService.login(dto);
@@ -42,7 +55,6 @@ class _LoginState extends State<LoginWidget> {
       await TokenService.saveToken(response.body);
 
       Navigator.pushReplacementNamed(context, homeRoute);
-
     } catch (e) {
       logger.e(e);
     }
@@ -52,58 +64,53 @@ class _LoginState extends State<LoginWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //todo: make own logo
-              const FlutterLogo(
-                size: 200,
-              ),
-              const SizedBox(height: sizedBoxHeight),
-              TextField(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //todo: make own logo
+                const FlutterLogo(
+                  size: 200,
+                ),
+                const SizedBox(height: _sizedBoxHeight),
+                TextFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter your email'),
+                    autofocus: true,
+                    controller: _emailController,
+                    validator: EmailValidator(errorText: 'Invalid email')),
+
+                const Divider(),
+                TextFormField(
+                  obscureText: true,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter your email'),
-                  onChanged: (text) {
-                    if (text != email) {
-                      setState(() {
-                        email = text;
-                      });
-                    }
-                  }),
-              const Divider(),
-              TextField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter your password'),
-                onChanged: (text) {
-                  if (text != password) {
-                    setState(() {
-                      password = text;
-                    });
-                  }
-                },
-              ),
-              const Divider(),
+                      labelText: 'Password',
+                      hintText: 'Enter your password'),
+                  controller: _passwordController,
+                  validator: RequiredValidator(errorText: 'Invalid password'),
+                ),
+                const Divider(),
 
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
-                onPressed: () => _login(context),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  onPressed: () => _login(context),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/auth/register_widget.dart
+++ lib/auth/register_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dct_client/services/auth_service.dart';
 import 'package:dct_client/services/token_service.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 import '../logger.dart';
 import '../routes.dart';
@@ -13,31 +14,44 @@ class RegisterWidget extends StatefulWidget {
 }
 
 class _RegisterState extends State<RegisterWidget> {
-  String email;
-  String password;
-  String name;
-  DateTime birthDate;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+  DateTime _birthDate;
 
   static const sizedBoxHeight = 30.0;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime timePicked = await showDatePicker(
       context: context,
-      initialDate: birthDate ?? DateTime.now(),
+      initialDate: _birthDate ?? DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2025),
     );
-    if (timePicked != null && timePicked != birthDate) {
+    if (timePicked != null && timePicked != _birthDate) {
       setState(() {
-        birthDate = timePicked;
+        _birthDate = timePicked;
       });
     }
   }
 
   Future<void> _register(BuildContext context) async {
     try {
-      final dto =
-          RegisterDto(email, password, name, birthDate.toIso8601String());
+      if (!_formKey.currentState.validate()) {
+        return;
+      }
+
+      final dto = RegisterDto(_nameController.text, _emailController.text,
+          _passwordController.text, _birthDate.toIso8601String());
 
       final response = await AuthService.register(dto);
 
@@ -59,6 +73,16 @@ class _RegisterState extends State<RegisterWidget> {
       Navigator.pushReplacementNamed(context, homeRoute);
     } catch (e) {
       logger.e(e);
+
+      //todo: make snackbar good
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Try again later',
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      );
     }
   }
 
@@ -66,106 +90,102 @@ class _RegisterState extends State<RegisterWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //todo: make own logo
-              const FlutterLogo(
-                size: 200,
-              ),
-              const SizedBox(height: sizedBoxHeight),
-              TextField(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView(
+              children: [
+                //todo: make own logo
+                const FlutterLogo(
+                  size: 200,
+                ),
+                const SizedBox(height: sizedBoxHeight),
+                TextFormField(
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Email',
                       hintText: 'Enter your email'),
-                  onChanged: (text) {
-                    if (text != email) {
-                      setState(() {
-                        email = text;
-                      });
-                    }
-                  }),
-              const Divider(),
-              TextField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter your password'),
-                onChanged: (text) {
-                  if (text != password) {
-                    setState(() {
-                      password = text;
-                    });
-                  }
-                },
-              ),
-              const Divider(),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Repeat password',
-                    hintText: 'Repeat your password'),
-              ),
-              const Divider(),
-              TextField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Name',
-                    hintText: 'Enter your name'),
-                onChanged: (text) {
-                  if (text != name) {
-                    setState(() {
-                      name = text;
-                    });
-                  }
-                },
-              ),
-              const Divider(),
+                  autofocus: true,
+                  controller: _emailController,
+                  validator: EmailValidator(errorText: 'Invalid email'),
+                ),
+                const Divider(),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter your password'),
+                  controller: _passwordController,
+                  validator: RequiredValidator(errorText: 'Invalid password'),
+                ),
+                const Divider(),
+                TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Repeat password',
+                        hintText: 'Repeat your password'),
+                    validator: (val) {
+                      if (val != _passwordController.text) {
+                        return 'Passwords Not Match';
+                      }
+                      return null;
+                    }),
+                const Divider(),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Name',
+                      hintText: 'Enter your name'),
+                  controller: _nameController,
+                  validator: RequiredValidator(errorText: 'Invalid name'),
+                ),
+                const Divider(),
 
-              // picker
+                // date picker
 
-              Text(
-                birthDate == null
-                    ? 'Pick date'
-                    : '${birthDate.day}/${birthDate.month}/${birthDate.year}',
-                style:
-                    const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: sizedBoxHeight,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.lightBlueAccent)),
-                onPressed: () => _selectDate(context),
-                child: const Text(
-                  'Select birth date',
-                  style: TextStyle(
-                    color: Colors.black,
+                Center(
+                  child: Text(
+                    _birthDate == null
+                        ? 'Pick date'
+                        : '${_birthDate.day}/${_birthDate.month}/${_birthDate.year}',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
-
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.lightGreen)),
-                onPressed: () => _register(context),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+                const SizedBox(
+                  height: sizedBoxHeight,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.lightBlueAccent)),
+                  onPressed: () => _selectDate(context),
+                  child: const Text(
+                    'Select birth date',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-              ),
-            ],
+
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.lightGreen)),
+                  onPressed: () => _register(context),
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
